718ea621d0ac51c5faf199cab3bab7d19ab4b21e
Opilane TTHK <opilane@tthk.ee>
2020-09-15 14:01:45 +0300

    first commit

---
+++ Switch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning!");
+            Console.WriteLine("What Day Of The Week Is It?");
+            string Day = Console.ReadLine().ToLower();
+            switch (Day)
+            {
+                case "sunday":
+                    Console.WriteLine("Garfield");
+                    break;
+                case "monday":
+                    Console.WriteLine("Shrek");
+                    break;
+                case "tuesday":
+                    Console.WriteLine("Kachaow");
+                    break;
+                case "wednesday":
+                    Console.WriteLine("Nut");
+                    break;
+                case "thursday":
+                    Console.WriteLine("Sanic");
+                    break;
+                case "friday":
+                    Console.WriteLine("Wazowski");
+                    break;
+                case "saturday":
+                    Console.WriteLine("Stinky");
+                    break;
+                default:
+                    Console.WriteLine("What Are You Doing?");
+                    break;
+            }
+        }
+    }
+}
---
+++ TestFeedback/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TestFeedback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning!");
+            Console.WriteLine("What Is Your Test Result?");
+            string Test = Console.ReadLine().ToLower();
+            switch (Test)
+            {
+                case "a":
+                    Console.WriteLine("Excellent");
+                    break;
+                case "b":
+                    Console.WriteLine("Very Good");
+                    break;
+                case "c":
+                    Console.WriteLine("Passed");
+                    break;
+                case "d":
+                    Console.WriteLine("Failed");
+                    break;
+                default:
+                    Console.WriteLine("Wasted");
+                    break;
+            }
+        }
+    }
+}
